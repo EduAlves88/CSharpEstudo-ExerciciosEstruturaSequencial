55ed296c63eadbf5788ae7a27330f8fb8d007b3d
Eduardo Alves <edu.alves88@outlook.com>
2021-08-04 15:07:15 -0300

    Add project files.

---
+++ ExerciciosEstruturaSequencial/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+
+//Faça um programa para ler dois valores inteiros,
+//e depois mostrar na tela a soma desses números com uma
+//mensagem explicativa, conforme exemplos.
+namespace ExerciciosEstruturaSequencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, soma;
+
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+
+            soma = num1 + num2;
+
+            Console.WriteLine("SOMA = " + soma);
+        }
+    }
+}
