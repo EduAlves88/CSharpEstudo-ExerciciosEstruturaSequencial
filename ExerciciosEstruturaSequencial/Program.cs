031372a54206a8d19dbb39fb58f2df26cc6c28e5
Eduardo Alves <edu.alves88@outlook.com>
2021-08-04 15:41:53 -0300

    Comit do exercicio de estrutura sequencial 2

--- ExerciciosEstruturaSequencial/Program.cs
+++ ExerciciosEstruturaSequencial/Program.cs
@@ -1,23 +1,37 @@
 ﻿using System;
+using System.Globalization;
 
-
-//Faça um programa para ler dois valores inteiros,
-//e depois mostrar na tela a soma desses números com uma
-//mensagem explicativa, conforme exemplos.
 namespace ExerciciosEstruturaSequencial
 {
     class Program
     {
+
+        
         static void Main(string[] args)
         {
+            //Faça um programa para ler dois valores inteiros,
+            //e depois mostrar na tela a soma desses números com uma
+            //mensagem explicativa, conforme exemplos.
             int num1, num2, soma;
-
             num1 = int.Parse(Console.ReadLine());
             num2 = int.Parse(Console.ReadLine());
-
             soma = num1 + num2;
-
             Console.WriteLine("SOMA = " + soma);
+
+            //Faça um programa para ler o valor do raio de um círculo,
+            //e depois mostrar o valor da área deste círculo com quatro
+            //casas decimais conforme exemplos.
+            //Fórmula da área: area = π.raio2
+            //Considere o valor de π = 3.14159
+            float area, raio;
+            raio = float.Parse(Console.ReadLine());
+            area = (float)3.14159 * (raio*raio);
+            Console.Write("Area do círculo = ");
+            Console.WriteLine(area.ToString("F4"), CultureInfo.InvariantCulture);
+
+
+
+
         }
     }
 }
