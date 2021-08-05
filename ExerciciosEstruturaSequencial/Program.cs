2fcd94c54407a34d5613297ca2a3ff06cedd6a63
Eduardo Alves <edu.alves88@outlook.com>
2021-08-05 15:23:26 -0300

    Lista 01 terminada com os exercícios do Nelio Alves.

--- ExerciciosEstruturaSequencial/Program.cs
+++ ExerciciosEstruturaSequencial/Program.cs
@@ -12,7 +12,9 @@ namespace ExerciciosEstruturaSequencial
             //e depois mostrar na tela a soma desses números com uma
             //mensagem explicativa, conforme exemplos.
             int num1, num2, soma;
+            Console.WriteLine("Entre com o valor 1: ");
             num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o valor 2: ");
             num2 = int.Parse(Console.ReadLine());
             soma = num1 + num2;
             Console.WriteLine("SOMA = " + soma);
@@ -23,6 +25,7 @@ namespace ExerciciosEstruturaSequencial
             //Fórmula da área: area = π.raio2
             //Considere o valor de π = 3.14159
             float area, raio;
+            Console.WriteLine("Entre com o Raio do círculo: ");
             raio = float.Parse(Console.ReadLine());
             area = (float)3.14159 * (raio * raio);
             Console.Write("Area do círculo = ");
@@ -33,9 +36,13 @@ namespace ExerciciosEstruturaSequencial
             //de A e B pelo produto de C e D
             //segundo a fórmula: DIFERENCA = (A * B - C * D).
             int v1, v2, v3, v4, difDoProd;
+            Console.WriteLine("Valor 1: ");
             v1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 2: ");
             v2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 3: ");
             v3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Valor 4: ");
             v4 = int.Parse(Console.ReadLine());
             difDoProd = (v1 * v2) - (v3 * v4);
             Console.Write("DIFERENÇA = ");
@@ -47,8 +54,11 @@ namespace ExerciciosEstruturaSequencial
             //mostre o número e o salário do funcionário, com duas casas decimais.
             int horaFuncionario, numFuncionario;
             float salarioTotal, salarioHora;
+            Console.WriteLine("Entre com o numero do funcionário: ");
             numFuncionario = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com as horas trabalhadas: ");
             horaFuncionario = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entre com o salário por hora: ");
             salarioHora = float.Parse(Console.ReadLine());
             salarioTotal = salarioHora * horaFuncionario;
             Console.WriteLine("Funcionário Número: " + numFuncionario);
@@ -62,11 +72,13 @@ namespace ExerciciosEstruturaSequencial
             //o valor unitário de cada peça 2.
             //Calcule e mostre o valor a ser pago.
             string entradaDeDados;
+            Console.WriteLine("Entre com o código da peça, o numero de peças e o valor unitário: ");
             entradaDeDados = Console.ReadLine();
-            string[] vetDados= entradaDeDados.Split(' ');
+            string[] vetDados = entradaDeDados.Split(' ');
             string codigoPeca1 = vetDados[0];
             int qtddePeca1 = int.Parse(vetDados[1]);
             float valorPeca1 = float.Parse(vetDados[2]);
+            Console.WriteLine("Entre com o código da peça, o numero de peças e o valor unitário: ");
             vetDados = Console.ReadLine().Split(' ');
             string codigoPeca2 = vetDados[0];
             int qtddePeca2 = int.Parse(vetDados[1]);
@@ -75,8 +87,45 @@ namespace ExerciciosEstruturaSequencial
             subTotal1 = (float)valorPeca1 * qtddePeca1;
             subTotal2 = (float)valorPeca2 * qtddePeca2;
             total = subTotal2 + subTotal1;
-            Console.Write("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
-           
+            Console.WriteLine("VALOR A PAGAR: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+
+            //Fazer um programa que leia três valores com ponto flutuante de dupla precisão: A, B e C.
+            //Em seguida, calcule e mostre:
+            //    a) a área do triângulo retângulo que tem A por base e C por altura.
+            //    b) a área do círculo de raio C. (pi = 3.14159)
+            //    c) a área do trapézio que tem A e B por bases e C por altura.
+            //    d) a área do quadrado que tem lado B.
+            //    e) a área do retângulo que tem lados A e B.
+            double resultado;
+            double pi = 3.14159;
+            string valFiguras;
+            Console.WriteLine("Entre com os valores de A, B e C: ");
+            valFiguras = Console.ReadLine();
+            string[] vetFiguras = valFiguras.Split(' ');
+            double a = double.Parse(vetFiguras[0]);
+            double b = double.Parse(vetFiguras[1]);
+            double c = double.Parse(vetFiguras[2]);
+
+
+            resultado = (a * c) / 2;
+            Console.WriteLine("Triangulo: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+            resultado = pi * (c * c);
+            Console.WriteLine("Círculo: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+            resultado = (a + b) / (2 * c);
+            Console.WriteLine("Trapézio: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+            resultado = b * b;
+            Console.WriteLine("Quadrado: " + resultado.ToString("F2", CultureInfo.InvariantCulture));
+
+            resultado = a * b;
+            Console.WriteLine("Retângulo: " + resultado);
+
+
+
+
+
         }
     }
 }
